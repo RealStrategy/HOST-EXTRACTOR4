f25dc9b187c24933ee1e59c0bd859a337a81e997
RealStrategy <36258164+RealStrategy@users.noreply.github.com>
2023-06-13 15:05:19 -0500

    READMEN.md

---
+++ READMEN.py
@@ -0,0 +1,53 @@
+# HOST-EXTRACTOR (Irparpayaña) V3.0
+
+Irparpayaña: The word comes from the Aymara language of Peru and its meaning is to accompany one until they are on their way.
+
+## PROJECT DATA
+
+- Sunday: 09/12/2018
+- Improvement Tuesday: 01/15/2019
+- Developer: real strategy / rwam
+- Updated: 03/01/2022 / All Real
+- New Update: 06/13/2023 / @RealStrategy
+
+## ONLINE EXTRACTOR
+
+- [Click Here](https://todoreal.com/link-extractor/)
+## MAIN GOAL
+
+Extracting links from web pages, as well as domains and subdomains, is one of the main features of this tool. In addition, it offers extensive options to get comprehensive information from websites. Its main use lies in the creation of free access to the Internet using VPN applications and payloads.
+
+## FUNCTION LIST
+
+- Host Extractor
+- Subdomain Extractor
+- DNS domain extractor (SlowDNS)
+- Check Host status (200/301/404)
+- Pro payload generator (Ws, Payload)
+- View open ports (Nmap,Port,Firewall)
+- View information of an IP (Geolocation)
+- Change script language
+
+## REQUIREMENTS
+
+- apt update && apt upgrade -y
+- apt install python3 -y
+- apt install nmap -y
+- apt install curl -y
+- apt install git -y
+- pip install requests
+
+## FACILITY
+
+- git clone https://github.com/RealStrategy/host-extractor4.git
+- cd host-extractor4
+- chmod +x menu.py
+- python3 menu.py
+
+## SOCIAL NETWORKS
+
+- CREATOR: [@RealStrategy](https://www.instagram.com/real_strategy)
+- TELEGRAM: https://t.me/RealHackRWAM
+- WEBSITE: https://todoreal.com
+- GITHUB: https://github.com/RealStrategy
+- FACEBOOK: https://www.facebook.com/todorealvip
